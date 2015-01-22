632bb96cad7a45ccad16c793178394a5a9e576a0
Abraham Escalante <aeklant@gmail.com>
2015-01-22 16:27:04 -0600

    Added the original first exercises

---
+++ hello_prompt.rb
@@ -0,0 +1,4 @@
+print 'Please enter your name: '
+name = gets.strip
+puts "Hello #{name}!"
+puts ("\n\t#{(1+2) * 3}")
---
+++ step1.rb
@@ -0,0 +1,6 @@
+total = 100
+abc = "the total is: "
+
+output = abc + total.to_s()
+
+puts (output)
